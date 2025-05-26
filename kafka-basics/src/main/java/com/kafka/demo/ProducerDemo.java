13949306cdaa094812f70b2ad020f7463de9554a
Rucha <ruchasheth62@gmail.com>
2025-05-26 10:37:26 +0530

    init commit

---
+++ kafka-basics/src/main/java/com/kafka/demo/ProducerDemo.java
@@ -0,0 +1,19 @@
+package com.kafka.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class);
+    public static void main(String[] args) {
+        System.out.println("I'm producer");
+        // create producer properties
+
+        // crate producer
+
+        // send data
+
+        // flush and close the Producer
+    }
+}
